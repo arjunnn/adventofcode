abbbde845e9e9a7b258a226267f0ca1f529bad99
Arjun Munji <arjunmunji@gmail.com>
2021-08-12 00:01:20 +0530

    day 3

---
+++ 2020/day03/day03.py
@@ -0,0 +1,28 @@
+import sys
+import os
+import re
+import math
+
+
+def get_input_lines_from_file():
+    with open("input", "r") as input_file:
+        return input_file.read().splitlines()
+
+
+if __name__ == "__main__":
+    input = get_input_lines_from_file()
+    right = 3
+    down = 1
+
+    def part_one(input):
+        next_index = 0
+        for row in input:
+            row = row.split('')
+            
+
+
+    def part_two(input):
+        pass
+
+    part_one(input)
+    part_two(input)
