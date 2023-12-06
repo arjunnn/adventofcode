0c6403d93363eff29563f0147d85b6be90b4621c
Arjun Munji <arjun.munji@uk.sunstar.com>
2023-12-06 21:51:33 +0000

    day 5 wip

--- 2023/day5/solution.py
+++ 2023/day5/solution.py
@@ -10,29 +10,26 @@ def part_one():
     lines = get_input_lines_from_file()
     seeds = [num for num in map(lambda seed: int(seed), lines[0].split(': ')[1].split(" "))]
     maps = {}
-    print(seeds)
+    print(f"seeds are {seeds}")
     line_index = 2
-    end_of_map = False
     current_map = ""
     while line_index < len(lines):
-        print(line_index, len(lines))
-        # if not lines[line_index]:
-        #     end_of_map = True
-        #     continue
         if lines[line_index].endswith("map:"):
+            print(f"line is {lines[line_index]}")
             current_map = lines[line_index].split(" ")[0]
             maps[current_map] = dict.fromkeys(["source", "destination"], dict.fromkeys(["name", "ranges"], list()))
             source, _, destination = current_map.split("-")
             maps[current_map]["source"]["name"] = source
             maps[current_map]["destination"]["name"] = destination
         elif lines[line_index]:
+            print(f"line is {lines[line_index]}")
             destination_range_start, source_range_start, range_length = (
                 val for val in map(lambda x: int(x), lines[line_index].split(" ")))
+            print(f"source start: {source_range_start}, destination start: {destination_range_start}, range length: {range_length}")
             maps[current_map]["source"]["ranges"].append(range(source_range_start, source_range_start + range_length))
             maps[current_map]["destination"]["ranges"].append(
                 range(destination_range_start, destination_range_start + range_length))
         line_index += 1
-        end_of_map = False
     print(maps)
 
 
